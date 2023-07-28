1765b7eb745604099fa699900ccf3b6224123ce9
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-07-28 23:12:23 +0200

    server:feat: Create basic server add models for categories/items/user/token add services add auth middleware add routes add uploads

---
+++ server/models/Item.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require('mongoose');
+
+const schema = new Schema({
+  name: {type: String, required: true},
+  price: {type: Number},
+  description: {type: String},
+  category: {type: Schema.Types.ObjectId, ref: 'Category'},
+  images: [{type: String}],
+  color: {type: String},
+  size: [{type: String}],
+  composition: [{type: String}],
+}, {
+  timestamps: true,
+});
+
+module.exports = model('Item', schema);
---
+++ server/routes/user.routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const User = require('../models/User');
+const auth = require('../middleware/auth.middleware');
+// eslint-disable-next-line new-cap
+const router = express.Router({mergeParams: true});
+
+router.patch('/:userId', auth, async (req, res) => {
+  try {
+    const {userId} = req.params;
+    // eslint-disable-next-line max-len
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {new: true});
+    res.send(updatedUser);
+  } catch (e) {
+    res.status(500).json({
+      message: 'Server error. Please try later...',
+    });
+  }
+});
+router.get('/:userId', auth, async (req, res) => {
+  try {
+    const {userId} = req.params;
+    const user = await User.findById(userId);
+    res.send(user);
+  } catch (e) {
+    res.status(500).json({
+      message: 'Server error. Please try later...',
+    });
+  }
+});
+module.exports = router;
