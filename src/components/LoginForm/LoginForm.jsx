3f32b75a6aa58b2b7d437acb5e0e20f53276e78e
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-05 14:17:45 +0200

    client:perf: install yup package

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './LoginForm.module.scss';
 
+// import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+
 const LoginForm = () => (
   <div className={styles.loginForm} data-testid="LoginForm">
-    LoginForm Component
+    <div className={styles.card}>
+      <h2>Login Form</h2>
+    </div>
   </div>
 );
 
