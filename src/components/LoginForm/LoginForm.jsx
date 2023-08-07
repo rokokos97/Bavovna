72a690edf4784ef69e4c09ee10f7938875028d19
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-07 10:49:40 +0200

    client:feat: add login form create component LoginForm.jsx add styles LoginForm.module.scss

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import styles from './LoginForm.module.scss';
+import * as Yup from 'yup';
+import {useFormik} from 'formik';
 
 
-const LoginForm = () => (
-  <div className={styles.loginForm} data-testid="LoginForm">
+const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Email is required'),
+      password: Yup.string().required('Password is required'),
+    }),
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
     <div className={styles.card}>
-      <h2>Login Form</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <div className={styles.field}>
+          <label htmlFor="email">Email address </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className={styles.error}>{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="password">Password </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.errors.password ? (
+            <div className={styles.error}>{formik.errors.password}</div>
+          ) : null}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginForm;
