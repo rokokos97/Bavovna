09b27c9cecb85b559289d791ed1e56377b77cb48
Vitalii-Mamchur <vetalpatsi@gmail.com>
2023-08-09 16:46:01 +0300

    client:style: corrected footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,22 +8,31 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerTop}>
         <div className={styles.footerTopLeft}>
-          <div className={styles.footerHelp}>
-            <h3>Help</h3>
-            <div className={styles.footerContacts}>
+          <div className={styles.footerContact}>
+            <h3>Contact</h3>
+            <div className={styles.footerList}>
               <Link to="tel:+380441234567">+38 (044) 123-45-67</Link>
               <Link to="mailto:help@email.com">help@email.com</Link>
-              <Link to="/">FAQ/Contact Us</Link>
+              <Link to="/">Contact Us</Link>
             </div>
           </div>
           <div className={styles.footerShop}>
             <h3>Shop</h3>
-            <div className={styles.footerShopList}>
+            <div className={styles.footerList}>
+              <Link to="/">Sale</Link>
+              <Link to="/">New</Link>
               <Link to="/">Shorts</Link>
               <Link to="/">Pants</Link>
               <Link to="/">T-Shirts</Link>
               <Link to="/">Tops</Link>
-              <Link to="/">SALE</Link>
+            </div>
+          </div>
+          <div className={styles.footerHelp}>
+            <h3>Help</h3>
+            <div className={styles.footerList}>
+              <Link to="/">Delivery information</Link>
+              <Link to="/">Return information</Link>
+              <Link to="/">FAQ</Link>
             </div>
           </div>
         </div>
@@ -44,7 +53,15 @@ const Footer = () => {
       </div>
       <div className={styles.footerBottom}>
         <div className={styles.footerBottomLeft}>
-          <p>© 2023 Bavovna | All Rights Reserved</p>
+          <p className={styles.footerBottomItem}>
+            © 2023 Bavovna | All Rights Reserved
+          </p>
+          <div className={styles.footerBottomItem}>
+            <Link to="/">Cookies settings</Link>
+          </div>
+          <div className={styles.footerBottomItem}>
+            <Link to="/">Privacy policy</Link>
+          </div>
         </div>
         <div className={styles.footerBottomRight}>
           <img src="/img/svg/liqpay.png" alt="liqpay" />
