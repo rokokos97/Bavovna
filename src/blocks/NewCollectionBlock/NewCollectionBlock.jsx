45fb68652528ac4db45dbdad1443690dcbb75f91
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-13 17:04:31 +0200

    client:style: add style NewCollectionBlock component

--- src/blocks/NewCollectionBlock/NewCollectionBlock.jsx
+++ src/blocks/NewCollectionBlock/NewCollectionBlock.jsx
@@ -4,16 +4,18 @@ import styles from './NewCollectionBlock.module.scss';
 const NewCollectionBlock = () => {
   return (
     <div className={styles.newCollectionBlock} data-testid="NewCollectionBlock">
-      <p className={styles.newCollectionBlock__title}>
-          new collection
-      </p>
-      <p className={styles.newCollectionBlock__manifest}>
-          Together, we can transform the way we consume
-          and create a brighter, greener future for generations to come
-      </p>
-      <button className={styles.buttons}>
-        <span className={styles.buttonText}>shop now</span>
-      </button>
+      <div className={styles.newCollectionBlock__wrapper}>
+        <p className={styles.newCollectionBlock__title}>
+            new collection
+        </p>
+        <p className={styles.newCollectionBlock__manifest}>
+            Together, we can transform the way we consume
+            and create a brighter, greener future for generations to come
+        </p>
+        <button className={styles.buttons}>
+          <span className={styles.buttonText}>shop now</span>
+        </button>
+      </div>
     </div>
   );
 };
