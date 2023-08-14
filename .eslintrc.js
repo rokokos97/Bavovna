54751c9feb2975c2325d2b4c1823e0bc39658aac
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-14 16:34:51 +0200

    client:refactor: refactor eslint.js config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,26 @@
 module.exports = {
+  // Оточення, для яких діють ці правила
   env: {
-    browser: true,
-    es2021: true,
+    browser: true, // Вказуємо, що це середовище браузера
+    es2021: true, // Включаємо підтримку ES2021
   },
-  extends: ['plugin:react/recommended', 'google'],
+  // Правила для перевірки коду, які ми використовуємо
+  extends: [
+    'plugin:react/recommended', // Включаємо плагін React
+    'google', // Використовуємо стандарти Google для стилю
+  ],
+  // За допомогою overrides ми можемо перезаписати або додати правила для конкретних файлів або папок
   overrides: [],
+  // Налаштування парсера для перетворення коду в AST (Abstract Syntax Tree)
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 'latest', // Версія ECMA-Script, яку ми використовуємо (остання на момент публікації коду)
+    sourceType: 'module', // Вказуємо, що ми використовуємо модулі
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    'require-jsdoc': ['error', {
-      'require': {
-        'FunctionDeclaration': false,
-        'MethodDefinition': false,
-        'ClassDeclaration': false,
-        'ArrowFunctionExpression': false,
-        'FunctionExpression': false,
-      },
-    }],
-    'max-len': ['error', {
-      'code': 120,
-    }],
+  // Підключені плагіни
   plugins: ['react'],
+  // Власні правила (overrides може також використовуватись для цього, але тут вони загальні для всього проєкту)
   rules: {
-    'linebreak-style': 0,
+    'linebreak-style': 0, // Відключаємо правило про стиль перенесення рядків
     'require-jsdoc': [
       'error',
       {
@@ -50,5 +33,6 @@ module.exports = {
         },
       },
     ],
+    'max-len': ['error', {'code': 120}],
   },
 };
