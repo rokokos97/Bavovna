26541cd1fe4135be8dc37d692e2b2992c2c33f13
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-15 22:49:58 +0200

    client:style: add styles to SaleBlock component

--- src/blocks/SaleBlock/SaleBlock.jsx
+++ src/blocks/SaleBlock/SaleBlock.jsx
@@ -3,7 +3,13 @@ import styles from './SaleBlock.module.scss';
 
 const SaleBlock = () => (
   <div className={styles.saleBlock} data-testid="SaleBlock">
-    SaleBlock Component
+    <div className={styles.contentBlock}>
+      <span className={styles.title}>summer sale</span>
+      <button className={styles.button}>
+        <span>shop now</span>
+      </button>
+    </div>
+
   </div>
 );
 
