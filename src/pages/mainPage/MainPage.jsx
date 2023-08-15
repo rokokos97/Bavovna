00c60061e822f01e504ce0ea557d1ddb1600b825
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-15 23:06:03 +0200

    client:feat: add BavovnaBlock component add component BavovnaBlock add styles to BavovnaBlock component

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -3,6 +3,7 @@ import styles from './MainPage.module.scss';
 import NewArrivalsBlock from '../../blocks/NewArrivalsBlock/NewArrivalsBlock';
 import NewCollectionBlock from '../../blocks/NewCollectionBlock/NewCollectionBlock';
 import SaleBlock from '../../blocks/SaleBlock/SaleBlock';
+import BavovnaBlock from '../../blocks/BavovnaBlock/BavovnaBlock';
 
 const MainPage = () => {
   return (
@@ -11,6 +12,7 @@ const MainPage = () => {
         <NewCollectionBlock />
         <NewArrivalsBlock />
         <SaleBlock />
+        <BavovnaBlock />
       </div>
     </>
   );
