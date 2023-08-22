fd002af0048fbe9b66392f6267022cd0c98102f8
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-22 15:23:11 +0200

    client:feat: add LoginForm component create LoginForm component add styles to LoginForm component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,7 +6,7 @@ import {NavLink} from 'react-router-dom';
 
 
 const LoginForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword] = useState(false);
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -20,56 +20,83 @@ const LoginForm = () => {
       console.log(JSON.stringify(values, null, 2));
     },
   });
-  const toggleShowPassword = () => {
-    setShowPassword((prevState) => !prevState);
-  };
+  //  const toggleShowPassword = () => {
+  //    setShowPassword((prevState) => !prevState);
+  //  };
   return (
-    <div className={styles.card}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className={styles.field}>
-          <label htmlFor="email">Email address </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email ? (
+    <div className={styles.loginForm}>
+      <div className={styles.titleBlock}>
+      Sign In
+        <span>
+            Welcome back! Please enter your details
+        </span>
+      </div>
+      <div className={styles.inputsBlock}>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          <div className={styles.input}>
+            <label htmlFor="email">
+              <span>
+                  Email *
+              </span>
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
             <div className={styles.error}>{formik.errors.email}</div>
           ) : null}
-        </div>
-        <div className={styles.field}>
-          <label htmlFor="password">Password </label>
-          <input
-            id="password"
-            name="password"
-            type={showPassword ? 'text' : 'password'}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
-          <button
-            className={styles.showPasswordBtn}
-            type='button'
-            onClick={toggleShowPassword}
-          >
-            show password
-          </button>
-          {formik.errors.password ? (
+          </div>
+          <div className={styles.input}>
+            <label htmlFor="password">
+              <span>
+                  Password *
+              </span>
+            </label>
+            <input
+              id="password"
+              name="password"
+              type={showPassword ? 'text' : 'password'}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+            {formik.errors.password ? (
             <div className={styles.error}>{formik.errors.password}</div>
           ) : null}
+          </div>
+          <button
+            className={styles.button}
+          >
+            <span>
+                Sign In
+            </span>
+          </button>
+        </form>
+        <div className={styles.socialButtonsBlock}>
+          <div className={styles.divider}>
+          </div>
+          <div className={styles.socialButton}>
+            <span>Sign in with Google</span>
+          </div>
+          <div className={styles.socialButton}>
+            <div>
+              <span>Sign in with Apple</span>
+            </div>
+          </div>
         </div>
-        <button type="submit">Log In</button>
-      </form>
+      </div>
       <p>
-        Dont have account?{'  '}
+      Dont have account?{'  '}
         <NavLink to="signUp"
           role="button"
         >
           {' '}
-          Sign Up
+        Sign Up
         </NavLink>
       </p>
 
