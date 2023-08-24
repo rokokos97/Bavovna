4fe91dc7d839588821ec70cda173de72546b72b4
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-24 15:38:39 +0200

    client:refactor: refactor validationSchema.js

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,10 +1,9 @@
 import * as Yup from 'yup';
-import {isValidNumber} from 'libphonenumber-js';
 
-export const validationSchema = Yup.object({
-  email: Yup.string()
-      .required('Email is required')
-      .email('Invalid email address'),
+export const validationSchema = Yup.object().shape({
+  confirmPassword: Yup.string()
+      .required('Password confirmation is required')
+      .oneOf([Yup.ref('password'), null], 'Passwords must match'),
   password: Yup.string()
       .required('Password is required')
       .min(8, 'Password has to be longer than 8 characters')
@@ -14,29 +13,15 @@ export const validationSchema = Yup.object({
       .matches(/(?=.*[A-Z])/, 'Password must contain at' +
         ' least one capital letter')
       .max(16, 'Password hasn\'t\' to be longer than 16 characters'),
-  confirmPassword: Yup.string()
-      .required('Password confirmation is required')
-      .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+  email: Yup.string()
+      .required('Email is required')
+      .matches(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Invalid email address'),
   name: Yup.string()
       .required('Name is required')
       .min(2, 'Name has to be longer than 2 characters')
       .matches(
-          /^[A-ZА-Я][a-zа-яA-ZА-Я]*$/,
+          /^[A-Z][a-z]+ [A-Z][a-z]+$/,
           'The name must begin with a capital' +
         ' letter and consist only of letters')
-      .max(15, 'Name must be at most 15 characters long'),
-  familyName: Yup.string()
-      .required('Name is required')
-      .min(2, 'Name has to be longer than 2 characters')
-      .matches(
-          /^[A-ZА-Я][a-zа-яA-ZА-Я]*$/,
-          'The name must begin with a capital' +
-        ' letter and consist only of letters')
-      .max(15, 'Name must be at most 15 characters long'),
-  phoneNumber: Yup.string()
-      .test('is-valid-number', 'Invalid phone number', (value) => {
-        if (!value) return true;
-        return isValidNumber(value);
-      })
-      .required('Phone number is required'),
+      .max(20, 'Name must be at most 15 characters long'),
 });
