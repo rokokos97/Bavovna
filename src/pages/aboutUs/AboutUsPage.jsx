704ac3dc0c7d89aadaf638005da33aa3f3609ef8
Vitalii-Mamchur <vetalpatsi@gmail.com>
2023-08-25 01:28:40 +0300

    client:feat: add AboutBlock for AboutUsPage

--- src/pages/aboutUs/AboutUsPage.jsx
+++ src/pages/aboutUs/AboutUsPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import AboutBlock from '../../blocks/AboutBlock/AboutBlock';
+import styles from './AboutUsPage.module.scss';
 
 const AboutUsPage = () => {
-  return <div>About us</div>;
+  return (
+    <section className={styles.aboutUs}>
+      <AboutBlock />
+      <div className={styles}></div>
+      <div className={styles}></div>
+    </section>
+  );
 };
 
 export default AboutUsPage;
-
