3ae811f66b1c289654e766aa5d1c6248a050ce59
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-08-29 20:08:15 +0200

    client:debug:generateAuthError: correct errors case

--- src/utils/generateAuthError.js
+++ src/utils/generateAuthError.js
@@ -2,7 +2,7 @@ export function generateAuthError(message) {
   switch (message) {
     case 'INVALID_PASSWORD':
       return 'Email or password not correct';
-    case 'EMAIL_EXISTS':
+    case 'EMAIL_EXIST':
       return 'User with this Email already exists';
     case 'EMAIL_NOT_FOUND':
       return 'User with this email was not found';
