f8af1cc5256a73efedbf531081c249f3f52d8a70
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-09-21 12:57:46 +0200

    server:feat:Change user Schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose');
 const schema = new Schema({
   name: {type: String},
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true, unique: true},
+  password: {type: String, unique: true},
   phoneNumber: {type: String},
   deliveryAddress: {type: Object},
   novaPoshtaAddress: {type: Object},
