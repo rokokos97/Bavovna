90914f31c2c1d19e674ecb9fc5e917dc6fb0a61c
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-09-24 22:35:34 +0200

    client:refactor:Add redirection to '/catalogue'

--- src/blocks/NewCollectionBlock/NewCollectionBlock.jsx
+++ src/blocks/NewCollectionBlock/NewCollectionBlock.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './NewCollectionBlock.module.scss';
+import {useNavigate} from 'react-router-dom';
 
 const NewCollectionBlock = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.newCollectionBlock} data-testid="NewCollectionBlock">
       <div className={styles.newCollectionBlock__wrapper}>
@@ -12,7 +14,12 @@ const NewCollectionBlock = () => {
             Together, we can transform the way we consume
             and create a brighter, greener future for generations to come
         </p>
-        <button className={styles.button}>
+        <button
+          className={styles.button}
+          onClick={()=>{
+            navigate('/catalogue');
+          }}
+        >
           <span>shop now</span>
         </button>
       </div>
