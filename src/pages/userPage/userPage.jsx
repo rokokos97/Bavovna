e2e3941f090b481dc0c5d55398c1c4d8c936d8bd
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-10-11 17:45:12 +0200

    client:feat:Create User Page create userPage component

---
+++ src/pages/userPage/userPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './userPage.module.scss';
+
+const UserPage = () => (
+  <div className={styles.userPage} data-testid="UserPage">
+    UserPage Component
+  </div>
+);
+
+export default UserPage;
