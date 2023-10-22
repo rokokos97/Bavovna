c55468c1cabfc71805f8082acce9d26a9650841d
Vitalii Mamchur <vetalpatsi@gmail.com>
2023-10-22 20:49:30 +0300

    client:feat: refactor footer

--- src/pages/helpPage/HelpPage.jsx
+++ src/pages/helpPage/HelpPage.jsx
@@ -1,31 +1,34 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import AsideHelpBlock from '../../blocks/AsideHelpBlock/AsideHelpBlock';
-import {helps} from '../../services/helps.service';
+import DeliveryBlock from '../../blocks/helpDescription/DeliveryBlock/DeliveryBlock/DeliveryBlock';
+import ReturnBlock from '../../blocks/helpDescription/ReturnBlock/ReturnBlock';
+import PaymentBlock from '../../blocks/helpDescription/PaymentBlock/PaymentBlock';
+import FaqBlock from '../../blocks/helpDescription/FaqBlock/FaqBlock';
+import PolicyBlock from '../../blocks/helpDescription/PolicyBlock/PolicyBlock';
+import CookiesBlock from '../../blocks/helpDescription/CookiesBlock/CookiesBlock';
 import styles from './HelpPage.module.scss';
+// import {useState} from 'react';
 
 const HelpPage = () => {
-  const [selectedHelpId, setSelectedHelpId] = useState(0);
-  console.log(selectedHelpId);
+  // const [activeHelp, setActiveHelp] = useState(0);
 
-  const handleHelpSelect = (id) => {
-    setSelectedHelpId(id);
-  };
-
-  const currentDescription = helps.find(
-      (help) => help.id === selectedHelpId,
-  ).description;
+  // const handleActiveHelp = (helpId) => setActiveHelp(helpId);
 
   return (
     <div className={styles.helpPage}>
       <div className={styles.helpAside}>
-        <AsideHelpBlock
-          helps={helps}
-          selectedHelpId={selectedHelpId}
-          handleHelpSelect={handleHelpSelect}
-        />
+        <AsideHelpBlock />
       </div>
       <div className={styles.helpDescription}>
-        {currentDescription}
+        <Routes>
+          <Route isActive path='delivery' element={<DeliveryBlock />} />
+          <Route path='return' element={<ReturnBlock />} />
+          <Route path='payment' element={<PaymentBlock />} />
+          <Route path='faq' element={<FaqBlock />} />
+          <Route path='cookies' element={<CookiesBlock />} />
+          <Route path='privacy' element={<PolicyBlock />} />
+        </Routes>
       </div>
     </div>
   );
