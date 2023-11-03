06d1a228a3eb5363268f59328b1f8f1885b3257f
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-11-03 22:59:32 +0100

    client:guestRoutes:refactor: Refactor. Add comments.

--- src/hoc/guestRoutes.js
+++ src/hoc/guestRoutes.js
@@ -1,22 +1,28 @@
 import {useNavigate} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import {useEffect} from 'react';
 import {getIsLoggedIn} from '../store/userSlice';
 import PropTypes from 'prop-types';
 
-
-const GuestRoutes = ({children}) => {
+// Компонент для захисту роутів призначених для неавторизованих користувачів
+const GuestRoutes = ({children, redirectTo = '/'}) => {
   const navigate = useNavigate();
   const isLoggedIn = useSelector(getIsLoggedIn);
 
-  if (!isLoggedIn) {
-    navigate('/');
-    return null;
-  }
-  return children;
+  // Редірект авторизованих користувачів з гостьових роутів
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(redirectTo);
+    }
+  }, [isLoggedIn, navigate, redirectTo]);
+
+  // Показуємо компонент тільки для неавторизованих користувачів
+  return !isLoggedIn ? children : null;
 };
 
 GuestRoutes.propTypes = {
   children: PropTypes.node.isRequired,
+  redirectTo: PropTypes.string, // Необов'язковий prop для налаштування шляху редіректу
 };
 
 export default GuestRoutes;
