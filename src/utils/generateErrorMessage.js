a69408e5b053a454a1768e877c886c46799e82c4
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-11-07 12:19:00 +0100

    client:refactor:Refactor code

--- src/utils/generateErrorMessage.js
+++ src/utils/generateErrorMessage.js
@@ -4,7 +4,7 @@ const transformErrorMessage = {
   EMAIL_NOT_FOUND_OR_INVALID_PASSWORD: 'The email address or password is incorrect.',
   EMAIL_NOT_FOUND: 'No account found with this email.',
   SERVER_ERROR: 'Oops... There was a server error with your connection, please try again later.',
-  EMAIL_SENT: 'We have sent you an email with further instructions on how to reset your password.',
+  RESET_EMAIL_SENT: 'We have sent you an email with further instructions on how to reset your password.',
   INVALID_CURRENT_PASSWORD: 'Current password is incorrect.',
 };
 export default transformErrorMessage;
