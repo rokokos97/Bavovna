54451f90f7136b8a7b5fc4e760031b88eb4331d2
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-11-17 13:53:40 +0100

    server:refactor:Change User Model add field currentDeliveryAddress

--- server/models/User.js
+++ server/models/User.js
@@ -9,7 +9,7 @@ const schema = new Schema({
   newPassword: {type: String},
   phoneNumber: {type: String},
   deliveryAddress: [{type: Object}],
-  novaPoshtaAddress: [{type: Object}],
+  currentDeliveryAddress: {type: Object},
   favorite: [{type: String}],
   orders: [{type: String}],
   isVerified: {type: Boolean},
