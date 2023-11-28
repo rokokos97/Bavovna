f50fdfa25ce208a9e41103c5d8bc91b1b971dd6b
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-11-28 12:53:14 +0100

    client:feat: create component shoppingCartPage

---
+++ src/pages/shoppingCartPage/shoppingCartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './shoppingCartPage.module.scss';
+
+const ShoppingCartPage = () => {
+  return (
+    <div className={styles.shoppingCartPage} data-testid="ShoppingCartPage">
+      ShoppingCartPage Component
+    </div>
+  );
+};
+
+export default ShoppingCartPage;
