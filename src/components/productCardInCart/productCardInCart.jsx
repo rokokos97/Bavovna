e601a24e5fa8ef188d9a67945402866c493f698b
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-12-02 11:50:54 +0100

    client:ItemPreviewCard:refactor: change redirect to Cart

--- src/components/productCardInCart/productCardInCart.jsx
+++ src/components/productCardInCart/productCardInCart.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './productCardInCart.module.scss';
+import DeleteIcon from '../svg/deleteIcon/deleteIcon';
+import PropTypes from 'prop-types';
 
-const ProductCardInCart = () => {
+const ProductCardInCart = ({item}) => {
+  console.log(item);
   return (
     <div className={styles.productCardInCart} data-testid="ProductCardInCart">
-
+      <div className={styles.infoBlock}>
+        <div className={styles.image}>
+          <img src={item.itemImg} alt='item picture'/>
+        </div>
+        <div className={styles.titleBlock}>
+          <p className={styles.title}>{item.itemName}</p>
+          <p className={styles.price}>{item.itemPrice} $</p>
+          <div>
+            <p>size: </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.deleteButton}>
+        <DeleteIcon />
+      </div>
     </div>
   );
 };
 
+ProductCardInCart.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 export default ProductCardInCart;
--- src/pages/shoppingCartPage/shoppingCartPage.jsx
+++ src/pages/shoppingCartPage/shoppingCartPage.jsx
@@ -1,39 +1,78 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './shoppingCartPage.module.scss';
 import LeftArrowIcon from '../../components/svg/leftArrowIcon/leftArrowIcon';
 import {useNavigate} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {getCart} from '../../store/cartSlice';
 import ProductCardInCart from '../../components/productCardInCart/productCardInCart';
+import {getItems, getItemsLoadingStatus} from '../../store/itemsSlice';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Autoplay} from 'swiper/modules';
+import ItemPreviewCard from '../../components/ItemPreviewCard/ItemPreviewCard';
 
 const ShoppingCartPage = () => {
   const cart = useSelector(getCart());
-  console.log(cart);
   const navigate = useNavigate();
+  const swiperRef = useRef();
+  const isItemsLoading = useSelector(getItemsLoadingStatus());
+  const items = useSelector(getItems());
+  let sliderItems = [];
+  if (!isItemsLoading && items) {
+    sliderItems = items.filter((item) => item.status === 'new');
+  }
   return (
     <div className={styles.shoppingCartPage} data-testid="ShoppingCartPage">
       <div className={styles.titleBlock}>
         <div className={styles.infoBlock}>
-          <p className={styles.title}>shopping bag (0 items)</p>
-          <p className={styles.text}>Your shopping bag is empty</p>
+          <p className={styles.title}>{`shopping bag (${cart.length} items)`}</p>
         </div>
-        <button
+        {(cart.length === 0) && <button
           className={styles.button}
           onClick={() => {
             navigate('/catalogue');
           }}
         >
-          <LeftArrowIcon />
+          <LeftArrowIcon/>
           <span>
             Continue Shopping
           </span>
-        </button>
+        </button>}
       </div>
-      <div>
-        {
-          cart.map((item) => <ProductCardInCart key={item._id} item={item}/>)
-        }
-      </div>
+      {cart.length !== 0 ?
+        <div className={styles.fillCartBlock}>
+          <div className={styles.itemsBlock}>
+            {
+              cart.map((item) => <ProductCardInCart key={item._id} item={item}/>)
+            }
+          </div>
+          <div className={styles.checkoutBlock}>
+
+          </div>
+        </div> :
+        <div className={styles.emptyCartBlock}>
+          <p className={styles.text}>Your shopping bag is empty</p>
+          {!isItemsLoading && sliderItems &&
+            <div className={styles.carusell}>
+              {/* Swiper для відображення елементів */}
+              <Swiper
+                onSwiper={(swiper) => swiperRef.current = swiper}
+                slidesPerView={3}
+                spaceBetween={3}
+                modules={[Autoplay]}
+                loop={true}
+                autoplay={{delay: 3000, pauseOnMouseEnter: true, stopOnLastSlide: true}}
+              >
+                {sliderItems.map((item) =>
+                  <SwiperSlide key={item._id}>
+                    <ItemPreviewCard id={item._id}/>
+                  </SwiperSlide>,
+                )}
+              </Swiper>
+            </div>
+          }
+        </div>
+
+      }
     </div>
   );
 };
