65bbe2fd39801ac1943868165590340e0fc65623
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-07 11:56:38 +0100

    client:style:Add style to orderSuccessPage

---
+++ src/pages/orderSuccessPage/orderSuccessPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './orderSuccessPage.module.scss';
+import {useSelector} from 'react-redux';
+import {getOrders, getOrdersLoadingStatus} from '../../store/ordersSlice';
+
+const OrderSuccessPage = () => {
+  const isLoadingOrders = useSelector(getOrdersLoadingStatus());
+  const orders = useSelector(getOrders());
+  return (!isLoadingOrders &&
+    <div className={styles.orderCompletePage} data-testid="OrderCompletePage">
+      <div className={styles.titleBlock}>
+        <p className={styles.title}>Your order has been confirmed</p>
+      </div>
+      <div className={styles.numberBlock}>
+        <p>order # {orders[0]._id}</p>
+        <p>date: {orders[0].date}</p>
+      </div>
+      <div className={styles.textBlock}>
+        <p>Thank you for shopping in our store!</p>
+        <p>You will receive an email notification when your order is shipped.</p>
+      </div>
+      <div className={styles.buttonsBlock}>
+        <button>
+          <span>
+            &lt;- go to shop
+          </span>
+        </button>
+        <button>
+          <span>
+            view order
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSuccessPage;
--- src/pages/orderSuccessPage/orderSuccessPage.test.js
+++ src/pages/orderSuccessPage/orderSuccessPage.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import OrderCompletePage from './orderCompletePage';
+import OrderSuccessPage from './orderSuccessPage';
 
 describe('<Box />', () => {
   test('it should mount', () => {
-    render(<OrderCompletePage />);
+    render(<OrderSuccessPage />);
 
     const orderCompletePage = screen.getByTestId('OrderCompletePage');
 
