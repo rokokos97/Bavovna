b5ecfe8046476e7c22bc761e9bb779a3f922df67
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-08 00:43:45 +0100

    client:refactor: Refactoring selectField.jsx

--- src/components/form/formFields/selectField/selectField.jsx
+++ src/components/form/formFields/selectField/selectField.jsx
@@ -4,7 +4,7 @@ import Select from 'react-select';
 import PropTypes from 'prop-types';
 
 
-const SelectField = ({defaultValue, onChange, options, label, error, touched}) => {
+const SelectField = ({defaultValue, onChange, options, label, error}) => {
   return (
     <div className={`${styles.selectField} ${error? styles.hasError: ''}`} data-testid="SelectField">
       <p className={styles.label}>{label}
@@ -18,7 +18,6 @@ const SelectField = ({defaultValue, onChange, options, label, error, touched}) =
         defaultValue={defaultValue}
         onChange={onChange}
         options={options}
-        touched={touched}
       />
       {error ? (
         <div className={styles.error}>{error}</div>
@@ -32,6 +31,5 @@ SelectField.propTypes = {
   options: PropTypes.array.isRequired,
   label: PropTypes.string,
   error: PropTypes.string,
-  touched: PropTypes.bool,
 };
 export default SelectField;
