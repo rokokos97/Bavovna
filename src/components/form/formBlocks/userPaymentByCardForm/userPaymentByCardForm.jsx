3e7a616d639913d401dadf8072abf43c8210615c
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-08 00:34:02 +0100

    client:feat: Change format card value

--- src/components/form/formBlocks/userPaymentByCardForm/userPaymentByCardForm.jsx
+++ src/components/form/formBlocks/userPaymentByCardForm/userPaymentByCardForm.jsx
@@ -4,13 +4,22 @@ import PropTypes from 'prop-types';
 import TextField from '../../formFields/textField/textField';
 
 const UserPaymentByCardForm = ({formik}) => {
+  const handleCardNumberChange = (e) => {
+    const {value} = e.target;
+    const formattedValue = value
+        .replace(/[^\d]/g, '')
+        .replace(/(.{4})/g, '$1-')
+        .trim()
+        .slice(0, 19);
+    formik.setFieldValue('cardNumber', formattedValue);
+  };
   return (
     <div className={styles.userPaymentByCardForm} data-testid="UserPaymentByCardForm">
       <TextField
         label='Card number'
         name='cardNumber'
         placeholder='Enter the card number'
-        onChange={formik.handleChange}
+        onChange={handleCardNumberChange}
         value={formik.values.cardNumber}
         error={formik.errors.cardNumber}
         onBlur={formik.handleBlur}
