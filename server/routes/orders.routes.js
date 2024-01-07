ff31d4dbc17d12fdb578005743853a213b992e9a
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-07 12:00:59 +0100

    server:refactor:Refactor orders routes

--- server/routes/orders.routes.js
+++ server/routes/orders.routes.js
@@ -1,18 +1,14 @@
 const express = require('express');
-// const auth = require('../middleware/auth.middleware');
 const Order = require('../models/Order');
 // eslint-disable-next-line new-cap
 const router = express.Router({mergeParams: true});
 
 router.post('/',
     async (req, res)=> {
-      console.log(req.body);
       const newOrder = await Order.create({
         ...req.body,
       });
-      await newOrder.save();
-      res.status(201).send({
-      });
+      res.status(201).send(newOrder);
     });
 
 module.exports = router;
