ed2ccdf46435000fb0ae5f0e595f013c825005fd
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-13 14:04:12 +0100

    client:debug: correct path

--- server/routes/item.routes.js
+++ server/routes/item.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Item = require('../models/Item');
 // eslint-disable-next-line new-cap
 const router = express.Router({mergeParams: true});
-const auth = require('../middleware/auth.middleware');
+//const auth = require('../middleware/auth.middleware');
 
 router.get('/', async (req, res) => {
   try {
