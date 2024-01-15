5f113be5e0f8799562319385267122d83cb5e6f3
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-15 22:44:16 +0100

    client:feat:Create modal window with network error

--- client/src/components/modal/modalContent/modalError/modalError.jsx
+++ client/src/components/modal/modalContent/modalError/modalError.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import styles from './modalError.module.scss';
 import CloseIcon from '../../../svg/closeIcon/CloseIcon';
 import PropTypes from 'prop-types';
+import AlertIcon from '../../../svg/alertIcon/alertIcon';
 
-const ModalError = ({handleCloseModal, error}) => (
+const ModalError = ({handleCloseModal}) => (
   <div className={styles.ModalError} data-testid="ModalCookies">
     <div className={styles.container}>
-      <p className={styles.content}>{error}</p>
+      <div className={styles.titleBlock}>
+        <AlertIcon/>
+        <p>Server Error</p>
+      </div>
       <button
         className={styles.closeButton}
         onClick={handleCloseModal}
-      ><CloseIcon/></button>
+      >
+        <CloseIcon/>
+      </button>
     </div>
+    <p className={styles.content}>
+      Unfortunately, we are experiencing some issues with our server.
+      We are working to resolve these issues and hope to restore normal service as soon as possible.
+      Please try again later. We apologize for any inconvenience
+    </p>
   </div>
 );
 ModalError.propTypes = {
