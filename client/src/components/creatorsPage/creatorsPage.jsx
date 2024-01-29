048909f8e299e01928e9bb37292d02731799369a
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-01-29 23:23:11 +0100

    client:feat:Add modalEducationProject on MainPage

---
+++ client/src/components/creatorsPage/creatorsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './CreatorsPage.module.scss';
+
+const CreatorsPage = () => (
+  <div className={styles.creatorsPage} data-testid="CreatorsPage">
+    CreatorsPage Component
+  </div>
+);
+
+export default CreatorsPage;
