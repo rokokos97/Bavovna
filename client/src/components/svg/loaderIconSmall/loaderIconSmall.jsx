56145f052140ef728ec532ef356a167e59000337
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-02-09 19:53:03 +0100

    client:feat:Create small loader for buttons

---
+++ client/src/components/svg/loaderIconSmall/loaderIconSmall.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './loaderIconSmall.module.scss';
+
+const LoaderIconSmall = () => (
+  <div className={styles.loaderIconSmall} data-testid="LoaderIconSmall">
+    {/* eslint-disable-next-line max-len */}
+    <svg width="32" height="32" viewBox="0 0 108 108" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Lines"><rect id="Rectangle 1" x="51.75" width="4.5" height="27" rx="2.25" fill="#99D2BD"/><rect id="Rectangle 5" y="56.25" width="4.5" height="27" rx="2.25" transform="rotate(-90 0 56.25)" fill="#5A9B83"/><rect id="Rectangle 7" x="28.9485" y="101.891" width="4.5" height="27" rx="2.25" transform="rotate(-150 28.9485 101.891)" fill="#1A6449"/><rect id="Rectangle 9" x="82.9485" y="99.6406" width="4.5" height="27" rx="2.25" transform="rotate(150 82.9485 99.6406)" fill="#AFAFAF"/><rect id="Rectangle 11" x="101.891" y="79.0518" width="4.5" height="27" rx="2.25" transform="rotate(120 101.891 79.0518)" fill="#C3C3C3"/><rect id="Rectangle 13" x="99.6406" y="25.0508" width="4.5" height="27" rx="2.25" transform="rotate(60 99.6406 25.0508)" fill="#F5F5F5"/><rect id="Rectangle 4" x="56.25" y="108.001" width="4.5" height="27" rx="2.25" transform="rotate(180 56.25 108.001)" fill="#103C2C"/><rect id="Rectangle 6" x="108" y="51.75" width="4.5" height="27" rx="2.25" transform="rotate(90 108 51.75)" fill="#D7D7D7"/><rect id="Rectangle 8" x="79.0518" y="6.10938" width="4.5" height="27" rx="2.25" transform="rotate(30 79.0518 6.10938)" fill="#A9DFCB"/><rect id="Rectangle 10" x="25.0518" y="8.35938" width="4.5" height="27" rx="2.25" transform="rotate(-30 25.0518 8.35938)" fill="#89C4AF"/><rect id="Rectangle 12" x="6.10938" y="28.9492" width="4.5" height="27" rx="2.25" transform="rotate(-60 6.10938 28.9492)" fill="#79B6A0"/><rect id="Rectangle 14" x="8.35938" y="82.9482" width="4.5" height="27" rx="2.25" transform="rotate(-120 8.35938 82.9482)" fill="#3A7F66"/></g></svg>
+  </div>
+);
+
+export default LoaderIconSmall;
