be2d175f91262edb0fbc6e02ca0969fc389294dd
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-02-25 22:22:21 +0100

    client:feat:SEO-optimize component NewCollectionBlock.jsxBlock.jsx

--- client/src/pages/mainPage/NewCollectionBlock/NewCollectionBlock.jsx
+++ client/src/pages/mainPage/NewCollectionBlock/NewCollectionBlock.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import styles from './NewCollectionBlock.module.scss';
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const NewCollectionBlock = () => {
-  const navigate = useNavigate();
   return (
-    <section className={styles.newCollectionBlock} data-testid="NewCollectionBlock">
-      <div className={styles.newCollectionBlock__wrapper}>
-        <p className={styles.newCollectionBlock__title}>
+    <article className={styles.newCollectionBlock} data-testid="NewCollectionBlock">
+      <img className={styles.newCollectionBlock__image}
+        src='/img/images/NewCollection/newCollection.jpeg'
+        alt="Interior view with a clothes rack, potted cacti,
+         and a framed motivational quote reading
+          'GET UP DRESS UP SHOW UP NEVER GIVE UP'"
+      />
+      <section className={styles.newCollectionBlock__section}>
+        <h1 className={styles.newCollectionBlock__title1}>
             new collection
-        </p>
-        <p className={styles.newCollectionBlock__manifest}>
+        </h1>
+        <p className={styles.newCollectionBlock__text}>
             Together, we can transform the way we consume
             and create a brighter, greener future for generations to come
         </p>
-        <button
-          className={styles.button}
-          onClick={()=>{
-            navigate('/shop');
-          }}
+        <Link
+          to='/shop'
+          aria-label="Shop the new collection now"
+          className={styles.newCollectionBlock__button}
         >
           <span>shop now</span>
-        </button>
-      </div>
-    </section>
+        </Link>
+      </section>
+    </article>
   );
 };
 
