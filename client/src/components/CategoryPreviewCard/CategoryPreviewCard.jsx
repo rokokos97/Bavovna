3e1fc08dd185b616fe47f5eaea74bf1e787d18d5
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-02-26 20:53:54 +0100

    server:feat: SEO - semantic optimization CategoriesBlock.jsx change cover image

--- client/src/components/CategoryPreviewCard/CategoryPreviewCard.jsx
+++ client/src/components/CategoryPreviewCard/CategoryPreviewCard.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import styles from './CategoryPreviewCard.module.scss';
 import PropTypes from 'prop-types';
 import config from '../../config.json';
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 const CategoryPreviewCard = ({item}) => {
-  const navigate = useNavigate();
   return (
-    <div
+    <Link
       className={styles.categoryPreviewCard}
       data-testid="CategoryPreviewCard"
-      onClick={()=> navigate(item.status ? `/shop?status=${item.status}`: `/shop?status=${item._id}`)}
+      to={item.status ? `/shop?status=${item.status}`: `/shop?status=${item._id}`}
     >
       <img src={`${config.apiEndpoint}${item.image}`} loading='lazy' alt="category cover image"/>
-      <p className={styles.title}>{item.name}</p>
-    </div>
+      <p className={styles.categoryPreviewCard__title}>{item.name}</p>
+    </Link>
   );
 };
 CategoryPreviewCard.propTypes = {
