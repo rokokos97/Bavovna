0175e43dd1897b17013fe5028702d8a3e192f377
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-02-26 20:36:30 +0100

    server:feat: SEO - semantic optimization SaleBlock.jsx change cover image

--- client/src/pages/mainPage/BavovnaTextBlock/BavovnaBlock.jsx
+++ client/src/pages/mainPage/BavovnaTextBlock/BavovnaBlock.jsx
@@ -5,17 +5,17 @@ import {Link} from 'react-router-dom';
 const BavovnaBlock = () => {
   return (
     <article className={styles.bavovnaBlock} data-testid="BavovnaBlock">
-      <header className={styles.titleBox}>
-        <h2 className={styles.title2}>bavovna is a ukrainian brand of</h2>
-        <h2 className={styles.title2}>sustainable clothes</h2>
+      <header className={styles.bavovnaBlock__titleBox}>
+        <h2 className={styles.bavovnaBlock__title2}>bavovna is a ukrainian brand of</h2>
+        <h2 className={styles.bavovnaBlock__title2}>sustainable clothes</h2>
       </header>
-      <p className={styles.content}>
+      <p className={styles.bavovnaBlock__content}>
         We are dedicated to bringing you a wide range of sustainable
         products that help reduce your ecological footprint without compromising on quality or style.
       </p>
       <Link
         to='/shop'
-        className={styles.button}>
+        className={styles.bavovnaBlock__button}>
         <span>
           shop now
         </span>
