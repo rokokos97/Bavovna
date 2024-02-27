85151337dc5133cf0d0ca67acb0bc8c3e3d6223d
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-02-27 10:59:34 +0100

    server:feat: SEO - semantic optimization Footer.jsx change cover image

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -28,81 +28,99 @@ const Footer = () => {
   }
   return (
     <footer className={styles.container} data-testid='Footer'>
-      <div className={styles.footer}>
-        <div className={styles.footerTop}>
-          <div className={styles.footerTopLeft}>
-            <div className={styles.footerContact}>
+      <article className={styles.footer}>
+        <section className={styles.footerTop}>
+          <section className={styles.footerTopLeft}>
+            <section className={styles.footerContact}>
               <h3>Contact</h3>
-              <div className={styles.footerList}>
-                <Link to='mailto:help@bavovna.space'>Contact Us</Link>
-                <Link to='/creators'>Project team</Link>
-              </div>
-            </div>
-            <div className={styles.footerShop}>
+              <nav className={styles.footerList}>
+                <Link to='mailto:help@bavovna.space' aria-label='Contact us by email'>Contact Us</Link>
+                <Link to='/creators' aria-label='Learn about the project team'>Project team</Link>
+              </nav>
+            </section>
+            <section className={styles.footerShop}>
               <h3>Shop</h3>
-              <div className={styles.footerList}>
-                <Link to='/shop?status=sale'>Sale</Link>
-                <Link to='/shop?status=new'>New</Link>
-                <Link to={`/shop?status=${getCategoryId('t-shirts')}`}>
+              <nav className={styles.footerList}>
+                <Link to='/shop?status=sale' aria-label='Shop sale items'>Sale</Link>
+                <Link to='/shop?status=new' aria-label='Shop new items'>New</Link>
+                <Link to={`/shop?status=${getCategoryId('t-shirts')}`} aria-label='Shop t-shirts category'>
                   T-Shirts
                 </Link>
-                <Link to={`/shop?status=${getCategoryId('dresses')}`}>
+                <Link to={`/shop?status=${getCategoryId('dresses')}`} aria-label='Shop dresses category'>
                   Dresses
                 </Link>
-                <Link to={`/shop?status=${getCategoryId('pants')}`}>Pants</Link>
-                <Link to={`/shop?status=${getCategoryId('skirts')}`}>
+                <Link to={`/shop?status=${getCategoryId('pants')}`} aria-label='Shop pants category'>Pants</Link>
+                <Link to={`/shop?status=${getCategoryId('skirts')}`} aria-label='Shop skirts category'>
                   Skirts
                 </Link>
-              </div>
-            </div>
-            <div className={styles.footerHelp}>
+              </nav>
+            </section>
+            <section className={styles.footerHelp}>
               <h3>Help</h3>
-              <div className={styles.footerList}>
-                <Link to='/help/delivery'>Delivery information</Link>
-                <Link to='/help/return'>Return information</Link>
-                <Link to='/help/payment'>Payment</Link>
-                <Link to='/help/faq'>FAQ</Link>
-              </div>
-            </div>
-          </div>
-          <div className={styles.footerTopRight}>
-            <button onClick={navigateToHome}>
+              <nav className={styles.footerList}>
+                <Link to='/help/delivery' aria-label='Learn about delivery options'>Delivery information</Link>
+                <Link to='/help/return' aria-label='Learn about return options'>Return information</Link>
+                <Link to='/help/payment' aria-label='Learn about payment options'>Payment</Link>
+                <Link to='/help/faq' aria-label='Learn FAQ'>FAQ</Link>
+              </nav>
+            </section>
+          </section>
+          <section className={styles.footerTopRight}>
+            <button
+              onClick={navigateToHome}
+              type='button'
+              aria-label='Return to homepage'
+            >
               <LogoIcon />
             </button>
-            <div className={styles.socialList}>
-              <a href='https://www.instagram.com/' target='_blank' className={styles.socialListItem} rel="noreferrer">
+            <nav className={styles.socialList}>
+              <a
+                href='https://www.instagram.com/'
+                target='_blank'
+                className={styles.socialListItem}
+                rel="noreferrer"
+                aria-label='Follow us on Instagram'>
                 <InstagramIcon />
               </a>
-              <a href='https://www.facebook.com/' target='_blank' className={styles.socialListItem} rel="noreferrer">
+              <a
+                href='https://www.facebook.com/'
+                target='_blank'
+                className={styles.socialListItem}
+                rel="noreferrer"
+                aria-label='Follow us on Facebook'>
                 <FacebookIcon />
               </a>
-              <a href='https://www.tiktok.com/' target='_blank' className={styles.socialListItem} rel="noreferrer">
+              <a href='https://www.tiktok.com/'
+                target='_blank'
+                className={styles.socialListItem}
+                rel="noreferrer"
+                aria-label='Follow us on TikTok'>
                 <TikTokIcon />
               </a>
-            </div>
-          </div>
-        </div>
-        <div className={styles.footerBottom}>
-          <div className={styles.footerBottomLeft}>
+            </nav>
+          </section>
+        </section>
+        <section className={styles.footerBottom}>
+          <section className={styles.footerBottomLeft}>
             <p className={styles.footerBottomItem}>
               © 2024 Bavovna | All Rights Reserved
             </p>
-            <div
+            <nav
               className={`${styles.footerBottomItem} ${styles.footerBottomCookies}`}
             >
-              <Link to='/help/cookies'>Cookies settings</Link>
-            </div>
+              <Link to='/help/cookies' aria-label='Learn about cookies'>Cookies settings</Link>
+            </nav>
             <div className={styles.footerBottomItem}>
-              <Link to='/help/privacy'>Privacy policy</Link>
+              <Link to='/help/privacy' aria-label='Learn about privacy'>Privacy policy</Link>
             </div>
-          </div>
-          <div className={styles.footerBottomRight}>
+          </section>
+          <section className={styles.footerBottomRight}>
             <img src='/img/svg/liqpay.png' alt='liqpay' />
             <img src='/img/svg/mastercard.svg' alt='mastercard' />
             <img src='/img/svg/visa.svg' alt='visa' />
-          </div>
-        </div>
-      </div>
+          </section>
+        </section>
+      </article>
     </footer>
   );
 };
