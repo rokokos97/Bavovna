12a256452f33b253eace69a22d10f4378c493284
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-03-17 10:18:43 +0100

    client:refactor: refactor and optimize validation schema.

--- client/src/utils/validationSchema.js
+++ client/src/utils/validationSchema.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 
 const streetNameRegex = /^[A-Z][a-z'-]*\s?([A-Z]?[a-z'-]*)?$/;
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[!_$%&*"])(?=.*[A-Z]).{8,}$/;
 export const validationSchemaRegisterForm = Yup.object().shape({
   license: Yup.boolean()
       .oneOf([true], 'You must agree to the terms and conditions')
@@ -10,16 +12,11 @@ export const validationSchemaRegisterForm = Yup.object().shape({
       .oneOf([Yup.ref('password'), null], 'Passwords must match'),
   password: Yup.string()
       .required('Password is required')
-      .min(8, 'Password has to be longer than 8 characters')
-      .matches(/(?=.*[0-9])/, 'Password must contain at least one number')
-      .matches(/(?=.*[!_$%&*"])/, 'Password must contain at ' +
-        'least one of symbols !_$%&*')
-      .matches(/(?=.*[A-Z])/, 'Password must contain at' +
-        ' least one capital letter')
+      .matches(passwordRegex, 'Password must contain at least 8 symbols long, include number, capital letter, special characters !_$%&*')
       .max(16, 'Password hasn\'t\' to be longer than 16 characters'),
   email: Yup.string()
       .required('Email is required')
-      .matches(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Invalid email address'),
+      .matches(emailRegex, 'Invalid email address'),
   lastName: Yup.string()
       .required('Last name is required')
       .min(2, 'Last name has to be longer than 2 characters')
@@ -41,21 +38,19 @@ export const validationSchemaLoginForm = Yup.object().shape({
   password: Yup.string()
       .required('Password is required'),
   email: Yup.string()
-      .matches( /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Email format is not valid')
-      .required('Email is required'),
+      .required('Email is required')
+      .matches( emailRegex, 'Email format is not valid'),
 });
 export const validationSchemaUserDataForm = Yup.object().shape({
   currentPassword: Yup.string(),
   newPassword: Yup.string()
-      .min(8, 'Password has to be longer than 8 characters')
-      .matches(/(?=.*[0-9])/, 'Password must contain at least one number')
-      .matches(/(?=.*[!_$%&*"])/, 'Password must contain at least one of symbols !_$%&*')
-      .matches(/(?=.*[A-Z])/, 'Password must contain at least one capital letter')
+      .required('Password is required')
+      .matches(passwordRegex, 'Password must contain at least 8 symbols long, include number, capital letter, special characters !_$%&*')
       .max(16, 'Password can\'t be longer than 16 characters'),
   phoneNumber: Yup.string()
       .matches(/^\d{12}$/, 'Phone number must be 12 digits'),
   email: Yup.string()
-      .email('Invalid email address'),
+      .matches(emailRegex, 'Invalid email address'),
   lastName: Yup.string()
       .min(2, 'Last name has to be longer than 2 characters')
       .matches(/^[A-Z][a-z]+$/, 'The Last name must begin with a capital letter and consist only of latin letters')
@@ -103,15 +98,19 @@ export const validationSchemaNPDeliveryInternational = Yup.object().shape({
 });
 export const validationSchemaPromoCode = Yup.object().shape({
   promoCode: Yup.string()
-      .min(5, 'Promo code must be at least 5 character long')
-      .max(15, 'Promo code must be at most 15 characters long'),
+      .test(
+          'len',
+          'Promo code must be between 1 and 15 characters long',
+          (val) => val && val.length >= 1 && val.length <= 15,
+      )
+      .matches(/^[A-Za-z0-9]+$/, 'Promo code must contain only letters and numbers'),
 });
 export const validationSchemaCheckOutUserInfo = Yup.object().shape({
   phoneNumber: Yup.string()
       .matches(/^\d{12}$/, 'Phone number must be 12 digits')
       .required('Phone number is required'),
   email: Yup.string()
-      .email('Invalid email address')
+      .matches(emailRegex, 'Invalid email address')
       .required('Email is required'),
   lastName: Yup.string()
       .min(2, 'Last name has to be longer than 2 characters')
@@ -137,7 +136,7 @@ export const validationSchemaCardPayment = Yup.object().shape({
 });
 export const validationSchemaNewsletterForm = Yup.object().shape({
   email: Yup.string()
-      .email('Invalid email address'),
+      .matches(emailRegex, 'Invalid email address'),
 });
 
 
