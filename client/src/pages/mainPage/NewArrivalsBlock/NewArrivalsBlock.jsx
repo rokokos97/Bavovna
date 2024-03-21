80e9fae352773d2f93fcd7d512a2596c6606ef3c
Vitalii-Mamchur <vetalpatsi@gmail.com>
2024-03-21 20:41:32 +0200

    client:style: added some styles from MainPage

--- client/src/pages/mainPage/NewArrivalsBlock/NewArrivalsBlock.jsx
+++ client/src/pages/mainPage/NewArrivalsBlock/NewArrivalsBlock.jsx
@@ -9,7 +9,6 @@ import 'swiper/css/navigation';
 import {Navigation} from 'swiper/modules';
 import BigLeftArrowIcon from '../../../components/svg/arrowIcons/BigLeftArrowIcon/BigLeftArrowIcon';
 
-
 const NewArrivalsBlock = () => {
   const swiperRef = useRef();
   const isItemsLoading = useSelector(getItemsLoadingStatus);
@@ -19,56 +18,61 @@ const NewArrivalsBlock = () => {
     newArrivalItems = items.filter((item) => item.status === 'new');
   }
   return (
-    <article className={styles.newArrivalsBlock} data-testid="NewArrivalsBlock">
+    <article className={styles.newArrivalsBlock} data-testid='NewArrivalsBlock'>
       <section className={styles.newArrivalsBlock__titleSection}>
-        <h2 className={styles.newArrivalsBlock__title2}>
-            new arrivals
-        </h2>
+        <h2 className={styles.newArrivalsBlock__title2}>new arrivals</h2>
         <section className={styles.newArrivalsBlock__arrowSection}>
           <button
             className={styles.newArrivalsBlock__button}
-            type = 'button'
+            type='button'
             aria-label='go to previous slide'
-            onClick={() => swiperRef.current?.slidePrev()}>
-            <BigLeftArrowIcon/>
+            onClick={() => swiperRef.current?.slidePrev()}
+          >
+            <BigLeftArrowIcon />
           </button>
           <button
             className={`${styles.newArrivalsBlock__button} ${styles.buttonRight}`}
-            type = 'button'
+            type='button'
             aria-label='go to previous slide'
-            onClick={() => swiperRef.current?.slideNext()}>
+            onClick={() => swiperRef.current?.slideNext()}
+          >
             <BigLeftArrowIcon />
           </button>
         </section>
       </section>
-      {!isItemsLoading && newArrivalItems &&
-              <Swiper
-                onSwiper={(swiper) => swiperRef.current = swiper}
-                modules={[Navigation]}
-                slidesPerView={1}
-                spaceBetween={1}
-                navigation={{
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
-                }}
-                breakpoints={{
-                  769: {
-                    slidesPerView: 3,
-                    spaceBetween: 3,
-                  },
-                  581: {
-                    slidesPerView: 2,
-                    spaceBetween: 2,
-                  },
-                }}
-              >
-                {newArrivalItems.map((item) =>
-                  <SwiperSlide key={item._id}>
-                    <ItemPreviewCard id={item._id}/>
-                  </SwiperSlide>,
-                )}
-              </Swiper>
-      }
+      {!isItemsLoading && newArrivalItems && (
+        <Swiper
+          className={styles.newArrivalsSwiper}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          modules={[Navigation]}
+          slidesPerView={2}
+          spaceBetween={8}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          breakpoints={{
+            769: {
+              slidesPerView: 3,
+              spaceBetween: 24,
+            },
+            581: {
+              slidesPerView: 2,
+              spaceBetween: 16,
+            },
+            501: {
+              slidesPerView: 3,
+              spaceBetween: 16,
+            },
+          }}
+        >
+          {newArrivalItems.map((item) => (
+            <SwiperSlide key={item._id}>
+              <ItemPreviewCard id={item._id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </article>
   );
 };
