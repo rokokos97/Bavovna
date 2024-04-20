e516b4c97cf7a4da35b9303c5dd2695b0c2208a8
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-04-20 23:05:32 +0200

    client:userPage:style: mobile adaptation OrdersBlock

--- client/src/pages/userPage/UserPage.jsx
+++ client/src/pages/userPage/UserPage.jsx
@@ -3,10 +3,6 @@ import styles from './UserPage.module.scss';
 import SideNavigation from './sideNavigation/SideNavigation';
 import {useDispatch} from 'react-redux';
 import {verifyUserEmail} from '../../store/userSlice';
-import {Link} from 'react-router-dom';
-import ChevronUp from '../../components/svg/ChevronUp/ChevronUp';
-
-
 const UserPage = () => {
   const dispatch = useDispatch();
   const urlParams = new URLSearchParams(window.location.search);
@@ -17,20 +13,6 @@ const UserPage = () => {
   }
   return (
     <section className={styles.userPage} data-testid="UserPage">
-      <nav
-        className={styles.userPage__navigation}>
-        <Link
-          className={styles.userPage__navigationHome}
-          to={'/'}>
-          <ChevronUp/>
-           Home&nbsp;
-        </Link>
-        <Link
-          li className={styles.userPage__navigationAccount}
-          to={''}>
-            / My account
-        </Link>
-      </nav>
       <SideNavigation/>
     </section>
   );
