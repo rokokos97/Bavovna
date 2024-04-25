5df9b6d3082d321b5323217c7bd944b08904daaa
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-04-25 17:44:26 +0200

    client:Show/HidePasswordIcon:style: add styles file

--- client/src/components/svg/passwordIcons/ShowPasswordIcon/ShowPasswordIcon.jsx
+++ client/src/components/svg/passwordIcons/ShowPasswordIcon/ShowPasswordIcon.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable max-len */
 import React from 'react';
+import styles from './ShowPasswordIcon.module.scss';
+
 const ShowPasswordIcon = () => (
-  <div>
+  <div className={styles.showPasswordIcon}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
