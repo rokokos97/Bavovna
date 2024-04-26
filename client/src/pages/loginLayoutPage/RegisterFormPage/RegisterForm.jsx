5a171118d51f60d3e63b2019142b9a4efdf66e4d
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-04-26 22:51:25 +0200

    client:RegisterForm:style: Mobile adaptation

--- client/src/pages/loginLayoutPage/RegisterFormPage/RegisterForm.jsx
+++ client/src/pages/loginLayoutPage/RegisterFormPage/RegisterForm.jsx
@@ -85,13 +85,15 @@ const RegisterForm = () => {
           </div> : null
       }
       <RegisterFormBlock formik={formik} googleRegister={googleRegister} isRegularSignUp={isRegularSignUp} isGoogleSignUp={isGoogleSignUp}/>
-      <p className={styles.toLoginForm}>
-        Already have an account?
+      <p className={styles.registerForm__toLoginFormLink}>
+        Already have an account?&nbsp;
         <NavLink
           to="/signIn"
+          title='go to sign in page'
+          aria-label='go to sign ip page'
           role="button"
         >
-          <span>&nbsp;Sign in</span>
+          Sign in
         </NavLink>
       </p>
       <Modal
